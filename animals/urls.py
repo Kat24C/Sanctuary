247a6d52d9c0a8b86ee8186eff78de6e8f4ba08c
Kat24C <kcsurvey47@gmail.com>
2023-02-12 17:58:30 +0000

    Add add, edit and delete view to animals/view.py

--- animals/urls.py
+++ animals/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('animals/', views.animal_outline, name='animal_outline'),
-    path('wild_animals/', views.wild_animals, name='wild_animals'),
-    path('farm_animals/', views.farm_animals, name='farm_animals'),
-    path('pets/', views.pets, name='pets'),
+    path('pet_info/<int:pet_id>', views.pet_info, name='pet_info'),
+    path('add_pet/', views.add_pet, name='add_pet'),
+    path('edit_pet/<int:pet_id>', views.edit_pet, name='edit_pet'),
+    path('delete_pet/<int:pet_id>', views.delete_pet, name='delete_pet'),
 ]
--- animals/views.py
+++ animals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, Http404, get_object_or_404, redirect, reverse
 from . import models
 from django.views import generic, View
 from django.contrib.auth.decorators import login_required
@@ -20,13 +20,92 @@ def animal_outline(request):
     return render(request, 'animals/animals_basic.html', context)
 
 
-def wild_animals(request):
-    return render(request, 'animals/animals_basic.html')
+def pet_info(request, pet_id):
+    pet = get_object_or_404(models.AboutTheAnimal, pk=pet_id)
+
+    context = {
+        'pet': pet
+    }
+    return render(request, 'animals/pet_info.html', context)
 
 
-def farm_animals(request):
-    return render(request, 'animals/animals_basic.html')
+@login_required
+def add_pet(request):
+    """ Add a new pet to the sanctuary """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff members can add new pets')
+        return redirect(reverse('animals_home_pg'))
+
+    if request.method == 'POST':
+        new_pet = PetDetails(request.POST, request.FILES)
+        if new_pet.is_valid():
+            pet = new_pet.save()
+            messages.success(request, 'A new pet has joined the sanctuary!')
+            return redirect(reverse('animal_outline'))
+        else:
+            messages.error(request, 'A new pet has not been add.')
+
+    else:
+        new_pet = PetDetails()
+
+    context = {
+        'new_pet': new_pet,
+    }
+
+    return render(request, 'animals/pet_add.html', context)
 
 
-def pets(request):
-    return render(request, 'animals/animals_basic.html')
+@login_required
+def edit_pet(request, pet_id):
+    """ Add a new pet to the sanctuary """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff members can access this!')
+        return redirect(reverse('animals_home_pg'))
+
+    try:
+        pet = models.AboutTheAnimal.objects.get(pk=pet_id)
+    except AboutTheAnimal.DoesNotExist:
+        messages.error(request, 'Sorry this did not work')
+        raise Http404("Oops. Sorry something went wrong.")
+
+    if request.method == 'POST':
+        pet_edit = PetDetails(request.POST, request.FILES, instance=pet)
+        if pet_edit.is_valid():
+            pet_edit.save()
+            messages.success(request, f'You have edited the \
+details of {pet.animals_name}')
+            return redirect(reverse('animal_outline'))
+    else:
+        pet_edit = PetDetails(instance=pet)
+        messages.info(request, f'You are changing the details of {pet.animals_name}')
+
+    context = {
+        'pet': pet,
+        'pet_edit': pet_edit,
+    }
+
+    return render(request, 'animals/animals_edit.html', context)
+
+
+def delete_pet(request, pet_id):
+    """ Add a new pet to the sanctuary """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff members can access this!')
+        return redirect(reverse('animals_home_pg'))
+
+    try:
+        pet = models.AboutTheAnimal.objects.get(pk=pet_id)
+    except AboutTheAnimal.DoesNotExist:
+        messages.error(request, "Oops. Sorry something went wrong.")
+        raise Http404("Oops. Sorry something went wrong.")
+
+    if request.method == "POST":
+        pet.delete()
+        messages.success(request, 'This pet has been adopted.')
+        return redirect(reverse('animal_outline'))
+
+    context = {
+        'pet': pet,
+    }
+
+    return render(request, 'animals/delete_pet.html', context)
