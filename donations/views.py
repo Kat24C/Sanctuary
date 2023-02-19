fb421336fd612f844d31e0a2170d2e9946c67846
Kat24C <kcsurvey47@gmail.com>
2023-02-19 17:18:50 +0000

    Work on views and template in donations

--- donations/views.py
+++ donations/views.py
@@ -14,22 +14,16 @@ def donation_payment(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     if request.method == 'POST':
         amount = int(request.POST['amount'])
-        charge = stripe.Charge.create(
-            amount=amount*100,
-            currency="gpb",
-            description="donation",
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {"amount": amount*100,
+                 "quantity": 1},
+            ],
+            mode="payment",
+            success_url="https://8000-kat24c-sanctuary-t3ec7b7nwuf.ws-eu87.gitpod.io/donations/success/",
+            cancel_url="https://8000-kat24c-sanctuary-t3ec7b7nwuf.ws-eu87.gitpod.io/sanctuary_home/index",
         )
-
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[
-            {"amount": amount,
-             "quantity": 1},
-        ],
-        mode="payment",
-        success_url="https://8000-kat24c-sanctuary-t3ec7b7nwuf.ws-eu87.gitpod.io/donations/success/",
-        cancel_url="https://8000-kat24c-sanctuary-t3ec7b7nwuf.ws-eu87.gitpod.io/sanctuary_home/index",
-    )
 
     context = {
         'session_id': session.id,
