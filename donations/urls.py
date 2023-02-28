4d0fbafda502489a0dc808588b0a9a5faac962a5
Kat24C <kcsurvey47@gmail.com>
2023-02-28 20:45:56 +0000

    Fix donation payment

--- donations/urls.py
+++ donations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.donations, name='donations'),
     path('donation_pay', views.donation_pay, name='donation_pay'),
-    path('donations_successful', views.donations_successful, name='donations_successful'),
-    path('stripe_webhook', views.stripe_webhook, name='stripe_webhook'),
+    path('success', views.success, name='success'),
+    path('wh/', webhook, name='webhook'),
 ]
--- donations/views.py
+++ donations/views.py
@@ -7,7 +7,9 @@ from django.contrib.auth.models import User
 
 
 import stripe
+import json
 
+stripe_public_key = settings.STRIPE_PUBLIC_KEY
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
@@ -20,56 +22,24 @@ def donation_cancelled(request):
 
 
 def donation_pay(request):
-    if request.method == "POST":
-        amount = request.POST['amount']
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price': amount,
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.build_absolute_uri(reverse('donations_successful')) + '?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url=request.build_absolute_uri(reverse('donation_cancelled')),
+    if request.method == 'POST':
+        print('Data:', request.POST)
+        amount = int(request.POST['amount'])
+
+        customer = stripe.Customer.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            source=request.POST['stripeToken'],
         )
 
-        return JsonResponse({
-            'session_id': session.id,
-            'stripe_public_key': settings.STRIPE_PUBLIC_KEY
-        })
-        
-
-def donations_successful(request):
-    return render(request, 'donations/donations_successful.html')
-
-
-@csrf_exempt
-def stripe_webhook(request):
-
-    print('WEBHOOK!')
-    # You can find your endpoint's secret in your webhook settings
-    endpoint_secret = 'whsec_Xj8wBk2qiUcjDEmYu5kfKkOrJCJ5UUjW'
-
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    event = None
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
+        charge = stripe.Charge.create(
+            customer=customer,
+            amount=amount*100,
+            currency='eur',
+            description='Donation',
         )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
+    return redirect('success')
 
-    # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-        print(session)
-        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
-        print(line_items)
 
-    return HttpResponse(status=200)
+def success(request):
+    return render(request, 'donations/success.html')
