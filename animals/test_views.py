8fa17395efbc6958ef629122db00cb8bcb47c71b
Kat24C <kcsurvey47@gmail.com>
2023-03-05 20:38:22 +0000

    Add styling to home and about

--- animals/test_views.py
+++ animals/test_views.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import AboutTheAnimal
+
 
 # Create your tests here.
+class TestViewsAnimals(TestCase):
+    def test_adoption_gets_pets(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'animals/pet_info.html')
--- sanctuary_home/test_views.py
+++ sanctuary_home/test_views.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from .models import HomePage
+from . import views
 # Create your tests here.
+
+
+class TestViews(TestCase):
+   
+    def test_animal_about_page(self):
+        """
+        Test to ensure all the information is shown
+        """
+
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sanctuary_home/about.html')
+
+    def test_edit_mission(self):
+        response = self.client.get(f'/edit_mission/{self.info.id}')
+        self.client.post(f'/edit_mission/{self.info.id}', {
+            'heading': 'Edited',
+            'statement': 'Test Statement',
+        })
+        edit = HomePage.objects.first().heading
+        self.assertEqual(edit, "Edited")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sanctuary_home/staff_form.html')
