46d94696a70f900e658920b3cd3a14e80e337cd2
Kat24C <kcsurvey47@gmail.com>
2023-03-06 17:46:26 +0000

    Update styling for animals templates

--- products/views.py
+++ products/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.core.mail import send_mail
 from django.conf import settings
-from django.views.generic import TemplateView
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponse
-from django.views import View
 from .models import Product
 
 import json
@@ -19,7 +14,7 @@ def donation_detail(request):
     """ A view to show individual product details """
 
     product = Product.objects.all()
-
+    messages.success(request, 'Please help us by donating')
     context = {
         'product': product,
         "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
